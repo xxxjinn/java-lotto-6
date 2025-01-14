adbb79471065e88b424a5000a1b7085583632f14
xxxjinn <tlsguswls319@naver.com>
2025-01-14 21:36:09 +0900

    당첨 번호 중복 안되게 에러처리

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,8 @@
 package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -18,6 +20,10 @@ public class Lotto {
             if (num < 1 || num > 45) {
                 throw new IllegalArgumentException("로또 번호는 1부터 45 사이의 숫자여야 합니다.");
             }
+        }
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException("로또 번호에는 중복이 존재하지 않아야 합니다.");
         }
     }
 
